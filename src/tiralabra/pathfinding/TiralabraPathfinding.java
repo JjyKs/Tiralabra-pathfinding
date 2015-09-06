6ee4f361caaf1acc90ababb325b29bd02c432abe
Jyri Saukkonen <jyri.saukkonen@gmail.com>
2015-09-06 21:13:51 +0300

    First commit

---
+++ src/tiralabra/pathfinding/TiralabraPathfinding.java
@@ -0,0 +1,17 @@
+
+package tiralabra.pathfinding;
+
+/**
+ *
+ * @author jjyks
+ */
+public class TiralabraPathfinding {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
